28553777ec14740e585f20baa988a69206ebb1cd
Anne van Dalfsen <avdalfsen@sogyo.nl>
2019-09-03 09:43:33 +0200

    Finished Tasks 1 and 2

---
+++ client/src/index.js
@@ -0,0 +1,36 @@
+function orderButtonClicked(event) {
+    let nameOfPark = event.target.parentNode.parentNode.getElementsByClassName("parkname")[0].innerText
+    let noOfAdults = parseInt(event.target.parentNode.getElementsByClassName("numberofadults")[0].value)
+    let noOfChildren = parseInt(event.target.parentNode.getElementsByClassName("numberofkids")[0].value)
+    saveOrderInShoppingBasket(nameOfPark, noOfAdults, noOfChildren)
+}
+
+setNumberOfOrders()
+
+function saveOrderInShoppingBasket(nameOfPark, noOfAdults, noOfChildren) {
+    let orderDataJSON = '{"parkName":'+nameOfPark+',"numberOfAdults":'+noOfAdults+',"numberOfChildren":'+noOfChildren+'}'
+    let counter = countOrders()
+    localStorage.setItem("order"+counter, orderDataJSON)
+
+    console.log(localStorage.getItem("order1"))
+    setNumberOfOrders()
+    
+}
+
+function countOrders(){
+    let counter = 0
+
+    while(localStorage.getItem("order"+counter)){
+        counter = counter + 1
+    }
+    return counter
+}
+
+function setNumberOfOrders() {
+    let counter = countOrders()
+    document.getElementsByClassName("badge")[0].innerHTML = counter;
+}
+
+
+let buttons = document.querySelectorAll("button.orderbutton")
+buttons.forEach(function (item) { item.addEventListener("click", orderButtonClicked) })
